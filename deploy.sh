8b1ed43c461d56e0a9aa8e52c27cd0f5b4c2d6f5
Egor Averyanov <egor.a2004@gmail.com>
2025-08-06 12:10:02 +0300

    terminus

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 distro=$(grep -o "Debian\|Arch" /etc/os-release | uniq)
 
 if [[ $distro == "Debian" ]]; then
-	sudo apt install -y alacritty
+	sudo apt install -y alacritty fonts-terminus
 elif [[ $distro == "Arch" ]]; then
-	sudo pacman -S --noconfirm --needed alacritty
+	sudo pacman -S --noconfirm --needed alacritty terminus-font
 fi
 
 ln -s $(pwd)/alacritty $HOME/.config/alacritty
