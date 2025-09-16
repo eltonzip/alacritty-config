7d160b14a96a40d30cae2803e8ebc2d574f3a2c9
Egor Averyanov <egor.a2004@gmail.com>
2025-09-16 19:31:20 +0300

    Better terminus

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ distro=$(grep -o 'Debian\|Arch' /etc/os-release | uniq)
 if [[ $distro == 'Arch' ]]; then
         sudo pacman -S --noconfirm --needed alacritty terminus-font
 elif [[ $distro == 'Debian' ]]; then
-        sudo apt install alacritty fonts-terminus
+        sudo apt install alacritty fonts-terminus-otb
 else
         echo 'this script is not suitable for your distro'
 fi
